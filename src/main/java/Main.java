865eb4ca39dcd8bf91822617a4d86ae96fb399bd
lukasz <stanislawczyk.lukasz@gmail.com>
2021-09-11 15:23:59 +0200

    the beginning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        String example = "Hello World!";
-        System.out.println(example);
-
-        MobilePhone phone = new MobilePhone("phone","500g",100);
-        MobilePhone oldPhone = new MobilePhone("old phone","400g",200);
-        MobilePhone newPhone = new MobilePhone("new phone");
-        phone.viewWalues();
-        System.out.println(phone.getValue());
+        Flight flight1 = new Flight("15:00","18:00");
+        System.out.println(flight1.getFlight());
     }
 }
 
-class MobilePhone{
-    String weight = "100g";
-    String name;
-    int price;
+class Flight {
+    String departure;
+    String arrival;
 
-    public MobilePhone(String name, String weight, int price) {
-        this.weight = weight;
-        this.name = name;
-        this.price = price;
+    public Flight(String departure, String arrival) {
+        this.departure = departure;
+        this.arrival = arrival;
     }
 
-    public MobilePhone(String name) {
-        this.name = name;
-    }
-    public void viewWalues(){
-        System.out.println("The weight of " + name + " is " +weight + ". The price is "+ price);
-    }
-
-
-    public String getValue() {
-        String all = "name " + name + " wegiht " + weight + " price " + price;
-        return all;
+    public String getFlight () {
+        return "Departure time: "+ departure + " Arrival time: " + arrival;
     }
 }
